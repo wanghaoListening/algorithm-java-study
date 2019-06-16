aea27e533d79dd86433dc8cb5cdbfcc021129426
王浩 <haowang162@creditease.cn>
2019-06-16 23:34:42 +0800

    由于中序遍历序列中，有1个数字是左子树结点的值，因此左子树总共有1个左子结点。同样，在前序遍历的序列中，

--- src/main/java/com/haothink/RebuildBinaryTree.java
+++ src/main/java/com/haothink/RebuildBinaryTree.java
@@ -7,13 +7,19 @@ package com.haothink;
  *
  * 根据二叉树的前序遍历和中序遍历的结果，重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
  *
- *
+ *{3,9,20,15,7}
+ *{9,3,15,20,7}
  *
  * 解题思路
  * 前序遍历的第一个值为根节点的值，使用这个值将中序遍历结果分成两部分，左部分为树的左子树中序遍历结果，
  * 右部分为树的右子树中序遍历的结果。
+ *
+ * 由于中序遍历序列中，有1个数字是左子树结点的值，因此左子树总共有1个左子结点。同样，在前序遍历的序列中，
+ * 根节点后面的1个数字就是1个左子树结点的值，再后面的所有数字都是右子树结点的值。这样我们就在前序遍历和
+ * 中序遍历两个序列中，分别找到了左右子树对应的子序列。
  */
 public class RebuildBinaryTree {
+
 
 
     public static void main(String[] args) {
@@ -24,6 +30,13 @@ public class RebuildBinaryTree {
 
 
     }
+
+
+    private static TreeNode constructBinaryTree(int[] preOrder,int[] middleOrder){
+
+        return null;
+    }
+
 
 
     private static class TreeNode{
