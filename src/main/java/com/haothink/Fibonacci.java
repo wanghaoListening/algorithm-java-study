639f575812f8943d208723a2c859ccc48871cf41
王浩 <haowang162@creditease.cn>
2019-06-24 23:44:17 +0800

    矩形覆盖

--- src/main/java/com/haothink/Fibonacci.java
+++ src/main/java/com/haothink/Fibonacci.java
@@ -13,7 +13,7 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
-
+        System.out.println(getValueByN3(36));
     }
 
     //递归求解
@@ -49,6 +49,26 @@ public class Fibonacci {
     }
 
     //考虑到第 i 项只与第 i-1 和第 i-2 项有关，因此只需要存储前两项的值就能求解第 i 项，从而将空间复杂度由 O(N) 降低为 O(1)。
+    public static int getValueByN3(int n){
+
+        if(n <= 1){
+            return n;
+        }
+
+        int fib = 0;
+        int fib0 = 0;
+        int fib1 = 1;
+
+        for(int i=2;i<=n;i++){
+
+            fib = fib0 + fib1;
+            fib0 = fib1;
+            fib1 = fib;
+        }
+        return fib;
+    }
+
+
 
 
 }
---
+++ src/main/java/com/haothink/RectangularCover.java
@@ -0,0 +1,41 @@
+package com.haothink;
+
+/**
+ * @author wanghao
+ * @date 2019年06月24日 23:15
+ * description: 矩形覆盖
+ *
+ * 我们可以用 2*1 的小矩形横着或者竖着去覆盖更大的矩形。请问用 n 个 2*1 的小矩形无重叠地覆盖一个 2*n 的大矩形，总共有多少种方法？
+ *
+ * 要覆盖 2*n 的大矩形，可以先覆盖 2*1 的矩形，再覆盖 2*(n-1) 的矩形；或者先覆盖 2*2 的矩形，再覆盖 2*(n-2) 的矩形。
+ * 而覆盖 2*(n-1) 和 2*(n-2) 的矩形可以看成子问题。该问题的递推公式如下：
+ */
+public class RectangularCover {
+
+
+    public static void main(String[] args) {
+        System.out.println(coverRectangular(10));
+    }
+
+
+    public static int coverRectangular(int n){
+
+        if(n <=2){
+            return n;
+        }
+
+        int fib1 = 1;
+        int fib2 = 2;
+        int fib = 0;
+
+        for(int i=3;i<=n;i++){
+
+            fib = fib1 +fib2;
+            fib1 = fib2;
+            fib2 = fib;
+
+        }
+        return fib;
+    }
+
+}
