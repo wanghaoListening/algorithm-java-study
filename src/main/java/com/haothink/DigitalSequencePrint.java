168e429013fd40cf594a893ef36e321d8706f21c
王浩 <haowang162@creditease.cn>
2019-07-06 19:32:26 +0800

    打印从 1 到最大的 n 位数

---
+++ src/main/java/com/haothink/DigitalSequencePrint.java
@@ -0,0 +1,13 @@
+package com.haothink;
+
+/**
+ * @author wanghao
+ * @date 2019年07月06日 19:31
+ * description: 打印从 1 到最大的 n 位数
+ * 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数即 999。
+ */
+public class DigitalSequencePrint {
+
+
+
+}
