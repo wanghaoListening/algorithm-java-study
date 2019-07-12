7c7b8cc2f8a00dbdfedff3d6e6c9e05ff129e488
wanghaoListening <haowang162@creditease.cn>
2019-07-12 18:37:05 +0800

    连续数的和

---
+++ src/main/java/com/haothink/bjtu/LxsSum.java
@@ -0,0 +1,38 @@
+package com.haothink.bjtu;
+
+/**
+ * @author wanghao
+ * @date 2019年07月12日 17:56
+ * description: 连续数的和
+ *
+ * 题面描述
+ * 对于一个给定的正整数 n ，请你找出一共有多少种方式使 n 表示为若干个连续正整数的和，要求至少包括两个正整数。如 n=15 时，可以有 3 种方式：（ 1+2+3+4+5 ），（ 4+5+6 ），（ 7+8 ）。
+ *
+ * 输入数据
+ * 输入数据第一行为一个正整数 T ，表示测试数据的组数。 随后的 T 行中，每行包括一组测试数据，为一个整数 n（1≤T≤1000,n≤10^9）。
+ *
+ * 输出数据
+ * 对每一组输入数据，输出一行结果 ”Case #id: M” ，表示第 id 组数据的结果是 M ， id 从 1 开始。
+ *
+ * 样例输入
+ * 2
+ * 3
+ * 15
+ *
+ * 样例输出
+ * Case #1: 1
+ * Case #2: 3
+ *
+ * https://blog.csdn.net/mengtech/article/details/1961799
+ */
+public class LxsSum {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
