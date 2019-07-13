59e6289cb32330e04c307146ca1b1f041a7b040c
wanghaoListening <haowang162@creditease.cn>
2019-07-13 15:39:23 +0800

    表示数值的字符串

---
+++ src/main/java/com/haothink/jzoffer/StrNumeric.java
@@ -0,0 +1,30 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年07月13日 15:34
+ * description: 表示数值的字符串
+ *
+ * true:
+ * "+100"
+ * "5e2"
+ * "-123"
+ * "3.1416"
+ * "-1E-16"
+ *
+ *
+ * false:
+ * "12e"
+ * "1a3.14"
+ * "1.2.3"
+ * "+-5"
+ * "12e+4.3"
+ *
+ * 参考：
+ * https://zhuanlan.zhihu.com/p/41347029
+ * https://github.com/CyC2018/CS-Notes/blob/master/notes/%E5%89%91%E6%8C%87%20Offer%20%E9%A2%98%E8%A7%A3%20-%2020~29.md#20-%E8%A1%A8%E7%A4%BA%E6%95%B0%E5%80%BC%E7%9A%84%E5%AD%97%E7%AC%A6%E4%B8%B2
+ */
+public class StrNumeric {
+
+
+}
