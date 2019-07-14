f18a5f328f5fa292d5ef47be5f3cdc8ce88ca8ed
wanghaoListening <haowang162@creditease.cn>
2019-07-14 20:35:01 +0800

    调整数组顺序使奇数位于偶数前面

---
+++ src/main/java/com/haothink/jzoffer/ChangePositionOfOddAndEven.java
@@ -0,0 +1,20 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年07月14日 20:32
+ * description: 调整数组顺序使奇数位于偶数前面
+ *
+ * 需要保证奇数和奇数，偶数和偶数之间的相对位置不变
+ *
+ * [1][2][3][4][5]
+ * [1][3][5][2][4]
+ *
+ * 方法一：创建一个新数组，时间复杂度 O(N)，空间复杂度 O(N)。
+ * 方法二：使用冒泡思想，每次都当前偶数上浮到当前最右边。时间复杂度 O(N2)，空间复杂度 O(1)，时间换空间。
+ */
+public class ChangePositionOfOddAndEven {
+
+
+
+}
