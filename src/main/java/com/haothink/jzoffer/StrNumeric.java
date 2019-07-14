eb9ccccfd34b9776303d3d93947ae3260ae5baed
wanghaoListening <haowang162@creditease.cn>
2019-07-14 20:24:10 +0800

    表示数值的字符串

--- src/main/java/com/haothink/jzoffer/StrNumeric.java
+++ src/main/java/com/haothink/jzoffer/StrNumeric.java
@@ -1,5 +1,7 @@
 package com.haothink.jzoffer;
 
+import java.util.Objects;
+
 /**
  * @author wanghao
  * @date 2019年07月13日 15:34
@@ -22,9 +24,108 @@ package com.haothink.jzoffer;
  *
  * 参考：
  * https://zhuanlan.zhihu.com/p/41347029
- * https://github.com/CyC2018/CS-Notes/blob/master/notes/%E5%89%91%E6%8C%87%20Offer%20%E9%A2%98%E8%A7%A3%20-%2020~29.md#20-%E8%A1%A8%E7%A4%BA%E6%95%B0%E5%80%BC%E7%9A%84%E5%AD%97%E7%AC%A6%E4%B8%B2
+ *
+ *
+ * 解题思路
+ * 设置三个标志符分别记录“+/-”、“e/E”和“.”是否出现过。
+ *
+ * 对于“+/-”： 正常来看它们第一次出现的话应该出现在字符串的第一个位置，如果它第一次出现在不是字符串首位，而且它的前面也不是“e/E”，那就不符合规则；
+ * 如果是第二次出现，那么它就应该出现在“e/E”的后面，如果“+/-”的前面不是“e/E”，那也不符合规则。
+ * 对于“e/E”： 如果它的后面不接任何数字，就不符合规则；如果出现多个“e/E”也不符合规则。
+ * 对于“.”： 出现多个“.”是不符合规则的。还有“e/E”的字符串出现“.”也是不符合规则的。 同时，要保证其他字符均为 0-9 之间的数字。
  */
 public class StrNumeric {
 
 
+    public static void main(String[] args) {
+
+        System.out.println(isNumeric("12e+4.3"));
+    }
+
+
+    private static boolean isNumeric(String strNumeric){
+
+        if(isEmpty(strNumeric)){
+            return false;
+        }
+
+        char[] chArray = strNumeric.toCharArray();
+        int numberOfE = 0;
+        int numberOfDot = 0;
+
+        for(int i=0;i<chArray.length;i++){
+
+            char ch = chArray[i];
+
+            if(ch == '+' || ch == '-'){
+
+                if(0 == i){
+                    continue;
+                }
+                if(chArray[i-1] == 'e' || chArray[i-1] == 'E'){
+                    continue;
+                }
+                return false;
+            }else if(ch == 'e' || ch == 'E'){
+                numberOfE ++;
+
+                if(0 == i){
+                    return false;
+                }
+                if(numberOfE > 1){
+                    return false;
+                }
+                if(numberOfDot >=1){
+                    return false;
+                }
+                if(i >= chArray.length-1){
+                    return false;
+                }
+                if(!isNumber(chArray[i+1]) && (!(chArray[i+1] == '-' || chArray[i+1] == '+'))){
+                    return false;
+                }
+
+            }else if(ch == '.'){
+                numberOfDot ++;
+                if(0 == i){
+                    return false;
+                }
+                if(numberOfDot >1){
+                    return false;
+                }
+
+                if(numberOfE >=1){
+                    return false;
+                }
+
+            }else{
+                if(!isNumber(chArray[i])){
+                    return false;
+                }
+            }
+
+        }
+
+
+        return true;
+    }
+
+    public boolean isNumeric2(String str) {
+
+        if(isEmpty(str)){
+            return false;
+        }
+
+        return str.matches("[+-]?\\d*(\\.\\d+)?([eE][+-]?\\d+)?");
+    }
+
+    private static boolean isEmpty(String str){
+
+        return Objects.isNull(str) || "".equals(str) || " ".equals(str);
+    }
+
+    private static boolean isNumber(char ch){
+        int num = Character.getNumericValue(ch);
+        return num >=0 && num <=9;
+    }
 }
