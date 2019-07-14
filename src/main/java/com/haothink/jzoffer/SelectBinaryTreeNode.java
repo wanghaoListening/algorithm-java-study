69a84563e739932b2503305f6392c690921c2835
wanghaoListening <haowang162@creditease.cn>
2019-07-14 20:55:15 +0800

    二叉树给定节点的下一个中序遍历节点

--- src/main/java/com/haothink/jzoffer/SelectBinaryTreeNode.java
+++ src/main/java/com/haothink/jzoffer/SelectBinaryTreeNode.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 /**
  * @author wanghao
  * @date 2019年06月20日 22:57
- * description:
+ * description: 二叉树给定节点的下一个中序遍历节点
  *
  * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
  * 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针
