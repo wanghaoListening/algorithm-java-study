1df8542b84cd04c0ab78654f24a0319eb5c9f694
wanghaoListening <haowang162@creditease.cn>
2019-07-15 19:39:08 +0800

    输入一个链表，输出该链表中倒数第k个结点。

---
+++ src/main/java/com/haothink/jzoffer/ReciprocalKNodeInlinked.java
@@ -0,0 +1,13 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年07月15日 19:38
+ * description: 输入一个链表，输出该链表中倒数第k个结点。
+ *
+ * 本题从1开始计数，即链表的尾结点是倒数第1个结点。例如一个链表有6个结点，从头结点开始它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个结点是值为4的结点。
+ */
+public class ReciprocalKNodeInlinked {
+
+
+}
