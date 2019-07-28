9e0a9a9fa845b56f080a76baef9941efcb27ec18
wanghaoListening <haowang162@creditease.cn>
2019-07-28 12:49:37 +0800

    二叉搜索树的后序遍历序列

---
+++ src/main/java/com/haothink/jzoffer/BiTreePostOrderSequence.java
@@ -0,0 +1,20 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年07月28日 12:48
+ * description: 二叉搜索树的后序遍历序列
+ *
+ * 题目描述：
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。假设输入的数组的任意两个数字都互不相同。
+ * 例如，下图是后序遍历序列 1,3,2 所对应的二叉搜索树。
+ */
+public class BiTreePostOrderSequence {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
