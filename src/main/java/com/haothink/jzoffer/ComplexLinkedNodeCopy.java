b8d26852b8c94fe869743653fb2a54778db9d02d
wanghaoListening <haowang162@creditease.cn>
2019-07-31 18:56:46 +0800

    复杂链表的复制

--- src/main/java/com/haothink/jzoffer/ComplexLinkedNodeCopy.java
+++ src/main/java/com/haothink/jzoffer/ComplexLinkedNodeCopy.java
@@ -10,7 +10,7 @@ import java.util.Objects;
  *
  * 题目描述：
  *
- * 题目：请实现函数 ComplexListNode clone(ComplexListNode head)，复制一个复杂链表。在复杂链表中，
+ * 题目：请实现函数 clone(ComplexListNode head)，复制一个复杂链表。在复杂链表中，
  * 每个结点除了有一个 next 域指向下一个结点外，还有一个 sibling 指向链表中的任意结点或者 null。
  *
  * 第一步，在每个节点的后面插入复制的节点。
