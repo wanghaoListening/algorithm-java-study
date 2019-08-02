63924d408c531633f49b60242e802edb535c8777
wanghaoListening <haowang162@creditease.cn>
2019-08-02 19:26:16 +0800

    序列化二叉树

---
+++ src/main/java/com/haothink/jzoffer/BiTreeSerializeAndDeserialize.java
@@ -0,0 +1,18 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年08月02日 17:45
+ * description: 序列化二叉树
+ *
+ * 题目描述：
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ *
+ * 解题思路：
+ *
+ */
+public class BiTreeSerializeAndDeserialize {
+
+
+
+}
