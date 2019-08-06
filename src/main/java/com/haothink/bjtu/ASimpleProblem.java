6fec683af02923fa1edca09307883e2647e2acaf
wanghaoListening <haowang162@creditease.cn>
2019-08-06 20:28:54 +0800

    小明的作业

---
+++ src/main/java/com/haothink/bjtu/ASimpleProblem.java
@@ -0,0 +1,46 @@
+package com.haothink.bjtu;
+
+/**
+ * @author wanghao
+ * @date 2019年08月06日 20:27
+ * description: A Simple Problem
+ *
+ * 题面描述
+ * 求 0~n 中有多少个整数 k 满足 (2 * k) XOR (3 * k) == k, 其中 XOR 为异或操作。
+ *
+ * 输入数据
+ * 输入数据的第一行为一个正整数 T(T<=30) ，表示测试数据的组数。
+ *
+ * 接下来的 T 行中，每行为一组测试数据，包含一个正整数 n(n<=1000000000) 。
+ *
+ * 输出数据
+ * 对每一组输入数据，输出一行结果 ”Case #id: M” ，表示第 id 组数据的结果是 M ， id 从 1 开始。
+ *
+ * 样例输入
+ * 5
+ * 0
+ * 1
+ * 2
+ * 3
+ * 4
+ *
+ * 样例输出
+ * Case #1: 1
+ * Case #2: 2
+ * Case #3: 3
+ * Case #4: 3
+ * Case #5: 4
+ *
+ *
+ */
+public class ASimpleProblem {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
--- src/main/java/com/haothink/bjtu/XiaoMingDeZuoYe.java
+++ src/main/java/com/haothink/bjtu/XiaoMingDeZuoYe.java
@@ -1,5 +1,11 @@
 package com.haothink.bjtu;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author wanghao
  * @date 2019年08月04日 17:33
@@ -9,7 +15,7 @@ package com.haothink.bjtu;
  * 小明每天都有很多作业要写，今天的作业内容是将一些分数写成小数的形式。众所周知，有些分数可以写成有限小数，比如 1 / 2 = 0.5 ，而有些分数会写成无限循环小数的形式，比如 1 / 3 = 0.33333 ……，小明觉得这个作业太麻烦了，于是请你来编程帮他完成这个作业。
  *
  * 输入数据
- * 输入数据的第一行为一个正整数 T ，表示测试数据的组数。接下来有 T 行测试数据，每行测试数据为空格分隔的两个正整数 a, b ，代表分数 a / b( 1 ≤ T ≤ 20, 0 ≤ a ≤ b) 。
+ * 输入数据的第一行为一个正整数 T ，表示测试数据的组数。接下来有T行测试数据，每行测试数据为空格分隔的两个正整数 a, b ，代表分数 a / b( 1 ≤ T ≤ 20, 0 ≤ a ≤ b) 。
  *
  * 输出数据
  * 对于每一组输入数据，输出一行结果。先输出” Case #id: ”，表示第 id 组数据， id 从 1 开始。如果该组数据能够写成有限小数，则在同一行中输出“ heiheihei ”，否则输出到该无限循环小数的第一个循环节结束。保证数据中所有无限小数均是 0.abcabc ……这样的形式，不会有形如 0.abcbc ……的小数，且循环节长度不超过 9 。
@@ -29,6 +35,39 @@ package com.haothink.bjtu;
  */
 public class XiaoMingDeZuoYe {
 
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int groupCount = Integer.parseInt(scan.nextLine());
+        List<String> results = new ArrayList<>(groupCount);
+        for(int i=0;i<groupCount;i++){
+
+            String[] str = scan.nextLine().split(" ");
+            BigDecimal bigA = new BigDecimal(str[0]);
+            BigDecimal bigB = new BigDecimal(str[1]);
+            if(isLoop(bigA,bigB)){
+                results.add(bigA.divide(bigB, 1, RoundingMode.DOWN).toString());
+            }else{
+                results.add("heiheihei");
+            }
+        }
+
+        for(int i=0;i<results.size();i++){
+            System.out.println("Case #"+(i+1)+": "+results.get(i));
+        }
+    }
+
+
+
+    public static boolean isLoop(BigDecimal bigA,BigDecimal bigB){
+        try{
+            bigA.divide(bigB);
+            return false;
+        }catch(Exception e){
+            return true;
+        }
+    }
 
 
 
