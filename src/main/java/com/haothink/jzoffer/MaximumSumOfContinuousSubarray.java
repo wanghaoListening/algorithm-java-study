b4eceba70f3f121ec02fb37251f475a39c80cac7
wanghaoListening <haowang162@creditease.cn>
2019-08-13 20:58:26 +0800

    连续子数组的最大和

---
+++ src/main/java/com/haothink/jzoffer/MaximumSumOfContinuousSubarray.java
@@ -0,0 +1,26 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年08月13日 20:30
+ * description: 连续子数组的最大和
+ *
+ * 题目描述：
+ * 输入一个整型数组，数组里有正数也有负数。数组中一个或连续的多个整数组成一个子数组。求所有子数组的和的最大值。要求时间复杂度为 O(n)。
+ *
+ * 例如：
+ * {6, -3, -2, 7, -15, 1, 2, 2}，连续子数组的最大和为 8（从第 0 个开始，到第 3 个为止）。
+ *
+ * 解题思路：
+ *
+ *
+ */
+public class MaximumSumOfContinuousSubarray {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
