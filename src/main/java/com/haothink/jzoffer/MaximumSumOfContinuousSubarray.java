4e7ee3971ed09f78b25206b658690a5e643d1f71
wanghaoListening <haowang162@creditease.cn>
2019-08-14 20:58:27 +0800

    连续子数组的最大和

--- src/main/java/com/haothink/jzoffer/MaximumSumOfContinuousSubarray.java
+++ src/main/java/com/haothink/jzoffer/MaximumSumOfContinuousSubarray.java
@@ -13,6 +13,12 @@ package com.haothink.jzoffer;
  *
  * 解题思路：
  *
+ * 可以利用动态规划的办法，用函数f(i)表示以第i个数字结尾的子数组的最大和，而我们需要求出max[f(i)]，其中0<=i<=n。
+ * 可以用如下递归公式求f(i)。
+ * 如果i=0，就是第一项；
+ * 如果f(i-1)<=0,那么就将舍弃前面的子数列，因为加起来会使后面的自数列的值<=自身子数列的值，所以舍弃。
+ * 如果i不等于0，且f(i-1)>0,那么它会使后面的自数列的值>自身子数列的值，所以保留。
+ *
  *
  */
 public class MaximumSumOfContinuousSubarray {
@@ -20,7 +26,26 @@ public class MaximumSumOfContinuousSubarray {
 
     public static void main(String[] args) {
 
+        int[] nums = {6, -3, -2, 7, -15, 1, 2, 2};
+        System.out.println(maxSubArray(nums));
     }
 
+
+    private static int maxSubArray(int[] nums){
+
+        int max = nums[0];
+        int sum = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+
+            sum = (sum <= 0)?nums[i]:sum+nums[i];
+
+            if(sum > max){
+                max = sum;
+            }
+        }
+
+        return max;
+    }
 
 }
