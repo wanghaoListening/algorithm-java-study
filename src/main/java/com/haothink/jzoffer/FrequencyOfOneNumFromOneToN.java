f48c8667936e7dac0fb4a6cf5012d2b7f291b9c4
wanghaoListening <haowang162@creditease.cn>
2019-08-15 19:53:39 +0800

    从 1 到 n 整数中 1 出现的次数

--- src/main/java/com/haothink/jzoffer/FrequencyOfOneNumFromOneToN.java
+++ src/main/java/com/haothink/jzoffer/FrequencyOfOneNumFromOneToN.java
@@ -5,6 +5,16 @@ package com.haothink.jzoffer;
  * @date 2019年08月14日 21:14
  * description: 从 1 到 n 整数中 1 出现的次数
  *
+ * 题目描述：
+ * 输入一个整数n，求1~n这n个整数的十进制表示中1出现的次数。
+ *
+ * 例子：
+ * 例如输入12,1~12这些整数中包含1的数字有1、10、11和12，1一共出现了5次。
+ *
+ * 解题思路：
+ * 这道题最容易想到的就是逐个遍历，然后判断每个数字包含1的个数，当然这样效率会比较低。
+ *
+ *
  *
  */
 public class FrequencyOfOneNumFromOneToN {
@@ -12,6 +22,9 @@ public class FrequencyOfOneNumFromOneToN {
     public static void main(String[] args) {
 
     }
+
+
+
 
 
 }
