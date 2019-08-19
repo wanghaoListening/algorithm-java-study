41cbee2eecef1413d940efc4b34036d282df35b4
wanghaoListening <haowang162@creditease.cn>
2019-08-19 19:53:12 +0800

    礼物的最大价值

---
+++ src/main/java/com/haothink/jzoffer/MaximumValueOfGifts.java
@@ -0,0 +1,27 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年08月19日 19:15
+ * description: 礼物的最大价值
+ *
+ * 题目描述：
+ * 在一个 m*n 的棋盘的每一个格都放有一个礼物，每个礼物都有一定价值（大于 0）。从左上角开始拿礼物，每次向右或向下移动一格，直到右下角结束。给定一个棋盘，求拿到礼物的最大价值。
+ *
+ * 1    10   3    8
+ * 12   2    9    6
+ * 5    7    4    11
+ * 3    7    16   5
+ *
+ * 礼物的最大价值为 1+12+5+7+7+16+5=53。
+ */
+public class MaximumValueOfGifts {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
