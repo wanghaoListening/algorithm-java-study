3583582d7661b65b0078aafab3adb83bc977ebe8
wanghaoListening <haowang162@creditease.cn>
2019-08-25 16:27:37 +0800

    丑数

---
+++ src/main/java/com/haothink/jzoffer/UglinessNumber.java
@@ -0,0 +1,25 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年08月25日 16:14
+ * description: 丑数
+ *
+ * 题目描述：
+ * 把只包含因子 2、3 和 5 的数称作丑数（Ugly Number）。例如 6、8 都是丑数，但 14 不是，因为它包含因子 7。习惯上我们把 1 当做是第一个丑数。求按从小到大的顺序的第 N 个丑数。
+ *
+ * 解题思路：
+ * 第一种：逐个判断每个数字是不是丑数的解法，直观但不够高效。
+ * 第二种：创建数组保存已经找到丑数，用空间换时间的解法。
+ */
+public class UglinessNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int getUglyNumberSolution(int n){
+
+
+    }
+}
