71235967a9e9bb54d161c7505a2ef54f3652a892
wanghaoListening <haowang162@creditease.cn>
2019-08-30 21:23:16 +0800

    数组中的逆序对

--- src/main/java/com/haothink/jzoffer/InversePairsInArray.java
+++ src/main/java/com/haothink/jzoffer/InversePairsInArray.java
@@ -1,5 +1,6 @@
 package com.haothink.jzoffer;
 
+
 /**
  * @author wanghao
  * @date 2019年08月27日 14:48
@@ -13,7 +14,10 @@ package com.haothink.jzoffer;
  *
  * 1.第一种比较容易想到的方法是逐个元素遍历跟后面的元素进行比较，由于每个数字都要和O(n)个数字作比较，因此这个算法的时间复杂度是O(n^2)
  *
- * 2.利用归并排序的思想，首先将
+ * 2.利用归并排序的思想，这里主要是对归并排序合并两个集合的过程进行改造，合并过程中我们对两个集合之间的逆序对进行统计并进行排序，例如
+ * 对(5,7) (4,6)两个集合进行合并过程中，分别设置两个指针i,j分别指向两个集合的最大元素，第一个集合的最大元素7大于第二个集合的最大元素6，
+ * 则出现的逆序对数为2，将7放入临时数组中，然后前一个集合的指针i向前移动一位，这时5小于6，则第二个集合的指针向前移动一位，发现前一个集合的
+ * 5大于后一个集合的4则出现逆序对，不断对集合合并过程的逆序对进行统计则最终集合合并完成的逆序对数量即为所求。
  *
  */
 public class InversePairsInArray {
@@ -24,7 +28,8 @@ public class InversePairsInArray {
 
         int[] nums = {7,5,6,4};
 
-        System.out.println(getInversePairs(nums));
+        getInversePairs2(nums);
+        System.out.println(counter);
     }
 
 
@@ -43,24 +48,49 @@ public class InversePairsInArray {
     }
 
 
-    private static int getInversePairs2(int[] nums){
 
-        
-        return 0;
+    //合并数组,对合并数组过程进行改造，统计逆序对(5,7) (4,6)
+    private static void merge(int[] nums,int low,int mid,int high){
+
+        int[] temp = new int[nums.length];
+        for(int k=low;k<=high;k++){
+            temp[k] = nums[k];
+        }
+        int i,j,k;
+        for(i=mid,j=high,k=high;i>=low&&j>=mid+1;k--){
+            if(temp[i] > temp[j]){
+                //前一个集合的元素大于后一个集合的元素则出现逆序对
+                counter+=(j-mid);
+                nums[k] = temp[i];
+                i--;
+            }else{
+                nums[k] = temp[j];
+                j--;
+            }
+        }
+
+        while(i>=low){nums[k--]=temp[i--];}
+        while(j>=mid+1){nums[k--]=temp[j--];}
+
     }
 
-    private static void merge(int[] nums,int stepLength){
+    //分割数组
+    private static void mergeSort(int[] nums,int low,int high){
 
-        if(stepLength >= nums.length){
-            return;
-        }
-        int stepCount = (nums.length%stepLength==0)?nums.length/stepLength:nums.length/stepLength+1;
-        int startIndex = 0;
-        int endIndex = startIndex+stepLength-1;
-        for(int i=1;i<=stepCount;i++){
-
-
+        if(low < high){
+            int mid = (low+high)/2;
+            mergeSort(nums,low,mid);
+            mergeSort(nums,mid+1,high);
+            merge(nums,low,mid,high);
         }
     }
+
+    private static void getInversePairs2(int[] nums){
+
+       int low = 0;
+       int high = nums.length-1;
+       mergeSort(nums,low,high);
+    }
+
 
 }
