c31152841cfd65e5aad561615b15953f4468c1a2
wanghaoListening <haowang162@creditease.cn>
2019-09-10 20:30:30 +0800

    滑动窗口的最大值

---
+++ src/main/java/com/haothink/jzoffer/MaxValueInWindow.java
@@ -0,0 +1,56 @@
+package com.haothink.jzoffer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author wanghao
+ * @date 2019年09月10日 20:09
+ * description: 滑动窗口的最大值
+ *
+ * 题目描述:
+ *
+ * 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，他们的最大值分别为{4,4,6,6,6,5}。
+ *
+ * 解题思路：
+ *
+ *
+ */
+public class MaxValueInWindow {
+
+    public static void main(String[] args) {
+
+        int[] num = {2,3,4,2,6,2,5,1};
+        System.out.println(maxValueInWindows(num,3));
+    }
+
+
+    private static List<Integer> maxValueInWindows(int[] num,int size){
+
+        int startIndex = 0;
+        int endIndex = size;
+
+        List<Integer> maxValueList = new ArrayList<>();
+        while (endIndex <= num.length){
+
+            int[] window = Arrays.copyOfRange(num,startIndex,endIndex);
+            maxValueList.add(selectMaxValue(window));
+            startIndex++;
+            endIndex++;
+        }
+        return maxValueList;
+    }
+
+    private static int selectMaxValue(int[] num){
+
+        int maxValue = num[0];
+        for(int i=1;i<num.length;i++){
+
+            if(num[i] > maxValue){
+                maxValue = num[i];
+            }
+        }
+        return maxValue;
+    }
+}
