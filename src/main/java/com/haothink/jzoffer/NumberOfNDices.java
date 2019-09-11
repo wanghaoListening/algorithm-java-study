18f58c72f285bab0ff7be9ab8ce45ad5905b9802
wanghaoListening <haowang162@creditease.cn>
2019-09-11 16:30:40 +0800

    n 个骰子的点数

---
+++ src/main/java/com/haothink/jzoffer/NumberOfNDices.java
@@ -0,0 +1,23 @@
+package com.haothink.jzoffer;
+
+/**
+ * @author wanghao
+ * @date 2019年09月11日 15:14
+ * description: n 个骰子的点数
+ *
+ * 题目描述：
+ * 把 n 个骰子扔在地上，求点数和为 s 的概率。
+ *
+ * 解题思路：
+ *
+ */
+public class NumberOfNDices {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
