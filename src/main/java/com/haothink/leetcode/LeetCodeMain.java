ecf76834aa1b21833433c0d9a102039608d34c7b
hiwanghao <hiwanghao@didiglobal.com>
2021-01-10 13:38:50 +0800

    开始刷leetcode的题目

---
+++ src/main/java/com/haothink/leetcode/LeetCodeMain.java
@@ -0,0 +1,12 @@
+package com.haothink.leetcode;
+
+/**
+ * Created by wanghao on 2021-01-10 mail:hiwanghao@didiglobal.com
+ **/
+public class LeetCodeMain {
+
+
+  public static void main(String[] args) {
+    System.out.println("hello leetcode");
+  }
+}
