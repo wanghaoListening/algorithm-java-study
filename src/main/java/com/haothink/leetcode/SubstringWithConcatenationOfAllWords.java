f7043d374d5bdb66fe3bdb4f8ea2d6bad278febf
hiwanghao <hiwanghao@didiglobal.com>
2021-03-11 21:10:19 +0800

    SubstringWithConcatenationOfAllWords

---
+++ src/main/java/com/haothink/leetcode/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,32 @@
+package com.haothink.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by wanghao on 2021-03-11 mail:
+ *
+ * You are given a string s and an array of strings words of the same length. Return all starting indices of substring(s) in s that is a concatenation of each word in words exactly once, in any order, and without any intervening characters.
+ *
+ * You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "barfoothefoobarman", words = ["foo","bar"]
+ * Output: [0,9]
+ * Explanation: Substrings starting at index 0 and 9 are "barfoo" and "foobar" respectively.
+ * The output order does not matter, returning [9,0] is fine too.
+ *
+ **/
+public class SubstringWithConcatenationOfAllWords {
+
+
+
+
+  public List<Integer> findSubstring(String s, String[] words) {
+
+    return null;
+  }
+
+}
