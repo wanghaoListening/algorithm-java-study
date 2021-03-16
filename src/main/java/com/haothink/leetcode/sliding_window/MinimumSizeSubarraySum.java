bb49af0f4da4f1be602e79c2129fc4bb1a770752
hiwanghao <hiwanghao@didiglobal.com>
2021-03-16 16:45:00 +0800

    Minimum Size Subarray Sum

---
+++ src/main/java/com/haothink/leetcode/sliding_window/MinimumSizeSubarraySum.java
@@ -0,0 +1,24 @@
+package com.haothink.leetcode.sliding_window;
+
+/**
+ * Created by wanghao on 2021-03-16 mail:
+ *
+ * Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray
+ * [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no such subarray,
+ * return 0 instead.
+ *
+ * Input: target = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+ **/
+public class MinimumSizeSubarraySum {
+
+
+  public int minSubArrayLen(int target, int[] nums) {
+
+
+
+    return 0;
+
+  }
+}
