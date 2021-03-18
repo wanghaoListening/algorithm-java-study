eb1edf9c914e8737dc60bce294f0b7311eae3363
hiwanghao <hiwanghao@didiglobal.com>
2021-03-18 17:28:46 +0800

    Longest Repeating Character Replacement

--- src/main/java/com/haothink/leetcode/sliding_window/LongestRepeatingCharacterReplacement.java
+++ src/main/java/com/haothink/leetcode/sliding_window/LongestRepeatingCharacterReplacement.java
@@ -3,21 +3,27 @@ package com.haothink.leetcode.sliding_window;
 /**
  * Created by wanghao on 2021-03-17
  *
- * Given a string s that consists of only uppercase English letters, you can perform at most k operations on that string.
+ * Given a string s that consists of only uppercase English letters, you can perform at most k
+ * operations on that string.
  *
- * In one operation, you can choose any character of the string and change it to any other uppercase English character.
+ * In one operation, you can choose any character of the string and change it to any other uppercase
+ * English character.
  *
- * Find the length of the longest sub-string containing all repeating letters you can get after performing the above operations.
+ * Find the length of the longest sub-string containing all repeating letters you can get after
+ * performing the above operations.
  *
- * Input:
- * s = "ABAB", k = 2
+ * Input: s = "ABAB", k = 2
  *
- * Output:
- * 4
+ * Output: 4
  *
- * Explanation:
- * Replace the two 'A's with two 'B's or vice versa.
+ * Explanation: Replace the two 'A's with two 'B's or vice versa.
  *
+ * "AABABBA" 1
+ *
+ * "AAAA"
+ * 0
+ *
+ * check corner case what the k ==0
  **/
 public class LongestRepeatingCharacterReplacement {
 
@@ -25,46 +31,43 @@ public class LongestRepeatingCharacterReplacement {
   public static void main(String[] args) {
 
     LongestRepeatingCharacterReplacement longestRepeatingCharacterReplacement = new LongestRepeatingCharacterReplacement();
-    String s = "ABAB";
-    int k = 2;
+    String s = "AAAA";
+    int k = 0;
 
-    System.out.println(longestRepeatingCharacterReplacement.characterReplacement(s,k));
+    System.out.println(longestRepeatingCharacterReplacement.characterReplacement(s, k));
   }
 
 
   public int characterReplacement(String s, int k) {
 
-
     int longestRepeatingCharacter = 0;
-
     char[] chs = s.toCharArray();
 
     int startIndex = 0;
     int endIndex = startIndex;
     int modifyCount = 0;
-    while (endIndex < chs.length){
+    while (endIndex < chs.length) {
 
-      if(modifyCount < k){
-
-        if(chs[startIndex] != chs[endIndex]){
-          modifyCount ++;
+      if(modifyCount >= k ||endIndex == chs.length-1 ) {
+        if ((endIndex - startIndex > longestRepeatingCharacter) || (longestRepeatingCharacter
+            == 0)) {
+          longestRepeatingCharacter = (startIndex==0)?(endIndex-startIndex+1):endIndex-startIndex;
         }
-        endIndex ++;
+        startIndex++;
+        endIndex = startIndex;
+        modifyCount = 0;
       }
 
-      if(modifyCount >= k){
-        //the count of modify up to k limit
-        if((endIndex-startIndex > longestRepeatingCharacter) || (longestRepeatingCharacter == 0)){
-
-          longestRepeatingCharacter = endIndex-startIndex;
-        }
-        startIndex ++;
-        endIndex = startIndex;
-
+      if(chs[startIndex] == chs[endIndex]) {
+        endIndex++;
+        continue;
+      }
+      if((chs[startIndex] != chs[endIndex]) && modifyCount < k) {
+        modifyCount++;
+        endIndex++;
       }
 
     }
-
     return longestRepeatingCharacter;
   }
 
