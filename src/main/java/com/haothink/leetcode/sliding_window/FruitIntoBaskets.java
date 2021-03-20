a6cc381a84ed6d15cf0d9249fe757da622bbc522
hiwanghao <hiwanghao@didiglobal.com>
2021-03-20 20:36:56 +0800

    Fruit Into Baskets

---
+++ src/main/java/com/haothink/leetcode/sliding_window/FruitIntoBaskets.java
@@ -0,0 +1,25 @@
+package com.haothink.leetcode.sliding_window;
+
+/**
+ * Created by wanghao on 2021-03-20 mail:
+ *
+ * 题目没看明白
+ **/
+public class FruitIntoBaskets {
+
+
+  public static void main(String[] args) {
+
+    FruitIntoBaskets fruitIntoBaskets = new FruitIntoBaskets();
+
+    int[] trees = {1,2,1};
+    System.out.println(fruitIntoBaskets.totalFruit(trees));
+  }
+
+  public int totalFruit(int[] tree) {
+
+
+
+    return 0;
+  }
+}
