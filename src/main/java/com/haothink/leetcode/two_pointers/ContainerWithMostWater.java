88a6cf95ff942867ec34dc2c1ab14a108ced60cd
hiwanghao <hiwanghao@didiglobal.com>
2021-03-21 11:22:51 +0800

    Container With Most Water

---
+++ src/main/java/com/haothink/leetcode/two_pointers/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package com.haothink.leetcode.two_pointers;
+
+/**
+ * Created by wanghao on 2021-03-21 mail:
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical
+ * lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which,
+ * together with the x-axis forms a container, such that the container contains the most water.
+ **/
+public class ContainerWithMostWater {
+
+
+
+  public int maxArea(int[] height) {
+
+
+    return 0;
+  }
+
+}
