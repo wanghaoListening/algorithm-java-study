4d0ac7a19a32d7089589aac55d0ab1e95e2560d3
wanghaoListening <wanghaotime@gmail.com>
2021-03-23 16:02:17 +0800

    Triplet Sum Closest

---
+++ src/main/java/com/haothink/leetcode/two_pointers/TripletSumClosest.java
@@ -0,0 +1,22 @@
+package com.haothink.leetcode.two_pointers;
+
+/**
+ * Created by wanghao on 2021-03-23
+ **/
+public class TripletSumClosest {
+
+
+  public static void main(String[] args) {
+
+    TripletSumClosest tripletSumClosest =new TripletSumClosest();
+
+
+  }
+
+
+  public int threeSumClosest(int[] nums, int target) {
+
+
+    return 0;
+  }
+}
