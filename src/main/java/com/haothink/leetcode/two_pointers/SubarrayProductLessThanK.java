6f99f2814603c3a862c6246ba50cedfa9bc1a6d2
wanghaoListening <wanghaotime@gmail.com>
2021-03-29 17:32:04 +0800

    Subarray Product Less Than K

--- src/main/java/com/haothink/leetcode/two_pointers/SubarrayProductLessThanK.java
+++ src/main/java/com/haothink/leetcode/two_pointers/SubarrayProductLessThanK.java
@@ -29,6 +29,12 @@ public class SubarrayProductLessThanK {
   public int numSubarrayProductLessThanK(int[] nums, int k) {
 
 
+    int count = 0;
+
+    for(int i=0;i<nums.length;i++){
+
+
+    }
 
     return 0;
   }
