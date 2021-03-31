8be35e8ae79381bd0f04c3680459f9c524081728
wanghaoListening <wanghaotime@gmail.com>
2021-03-31 21:46:15 +0800

    Remove Nth Node From End Of List

---
+++ src/main/java/com/haothink/leetcode/slow_and_fast_pointers/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,33 @@
+package com.haothink.leetcode.slow_and_fast_pointers;
+
+/**
+ * Created by wanghao on 2021-03-31 mail
+ *
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ **/
+public class RemoveNthNodeFromEndOfList {
+
+  public static void main(String[] args) {
+
+  }
+
+
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    //create double pointer
+    ListNode before;
+    ListNode after;
+
+
+    return null;
+  }
+
+
+  public static class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
