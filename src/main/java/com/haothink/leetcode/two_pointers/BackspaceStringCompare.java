7c7a0e025c34bd28d19763b3ebfb14eb8f45022d
wanghaoListening <wanghaotime@gmail.com>
2021-03-31 16:11:40 +0800

    Backspace String Compare

--- src/main/java/com/haothink/leetcode/two_pointers/BackspaceStringCompare.java
+++ src/main/java/com/haothink/leetcode/two_pointers/BackspaceStringCompare.java
@@ -1,5 +1,9 @@
 package com.haothink.leetcode.two_pointers;
 
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by wanghao on 2021-03-30 mail
  *
@@ -13,6 +17,15 @@ package com.haothink.leetcode.two_pointers;
  * 输出：true
  * 解释：S 和 T 都会变成 “ac”。
  *
+ * "a##c"
+ * "#a#c"
+ *
+ * "a#c"
+ * "b"
+ *
+ * "isfcow#"
+ * "isfco#w#"
+ *
  **/
 public class BackspaceStringCompare {
 
@@ -21,8 +34,8 @@ public class BackspaceStringCompare {
 
     BackspaceStringCompare backspaceStringCompare = new BackspaceStringCompare();
 
-    String s = "ab#c";
-    String t = "ad#c";
+    String s = "isfcow#";
+    String t = "isfco#w#";
 
     System.out.println(backspaceStringCompare.backspaceCompare(s,t));
   }
@@ -30,9 +43,55 @@ public class BackspaceStringCompare {
 
   public boolean backspaceCompare(String s, String t) {
 
+    char[] sch = s.toCharArray();
+    char[] tch = t.toCharArray();
 
+    int size = Math.max(sch.length,tch.length);
+    int start = 0;
 
-    return false;
+    List<Character> schStack = new LinkedList<>();
+    List<Character> tchStack = new LinkedList<>();
+    while (start < size){
+
+      if(start < sch.length) {
+        if (sch[start] != '#') {
+
+          schStack.add(sch[start]);
+        } else {
+          // remove the last ele
+          if (schStack.size() >= 1) {
+            schStack.remove(schStack.size() - 1);
+          }
+        }
+      }
+
+      if(start < tch.length) {
+        if (tch[start] != '#') {
+          tchStack.add(tch[start]);
+        } else {
+          // remove the last ele
+          if (tchStack.size() >= 1) {
+            tchStack.remove(tchStack.size() - 1);
+          }
+        }
+      }
+      start ++;
+    }
+
+    if(schStack.size() !=tchStack.size() ){
+
+      return false;
+    }
+
+    for(int i=0;i<schStack.size();i++){
+
+      if(schStack.get(i) != tchStack.get(i)){
+
+        return false;
+      }
+    }
+
+    return true;
 
   }
 }
