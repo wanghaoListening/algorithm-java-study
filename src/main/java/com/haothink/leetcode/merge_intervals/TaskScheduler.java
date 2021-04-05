7826b4f565981d71f9302486243f4c84eaee2839
wanghaoListening <wanghaotime@gmail.com>
2021-04-05 19:15:28 +0800

    Task Scheduler

--- src/main/java/com/haothink/leetcode/merge_intervals/TaskScheduler.java
+++ src/main/java/com/haothink/leetcode/merge_intervals/TaskScheduler.java
@@ -2,6 +2,22 @@ package com.haothink.leetcode.merge_intervals;
 
 /**
  * Created by wanghao on 2021-04-05
+ *
+ * Given a characters array tasks, representing the tasks a CPU needs to do, where each letter
+ * represents a different task. Tasks could be done in any order. Each task is done in one unit
+ * of time. For each unit of time, the CPU could complete either one task or just be idle.
+ *
+ * However, there is a non-negative integer n that represents the cooldown period between two
+ * same tasks (the same letter in the array), that is that there must be at least n units of
+ * time between any two same tasks.
+ *
+ * Return the least number of units of times that the CPU will take to finish all the given tasks.
+ *
+ * Input: tasks = ["A","A","A","B","B","B"], n = 2
+ * Output: 8
+ * Explanation:
+ * A -> B -> idle -> A -> B -> idle -> A -> B
+ * There is at least 2 units of time between any two same tasks.
  **/
 public class TaskScheduler {
 
