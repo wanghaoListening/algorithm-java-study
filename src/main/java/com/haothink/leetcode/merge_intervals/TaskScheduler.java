b905fcd855f4956289a17b2b4dc0c5ccfe800994
wanghaoListening <wanghaotime@gmail.com>
2021-04-09 16:05:44 +0800

    Task Scheduler

--- src/main/java/com/haothink/leetcode/merge_intervals/TaskScheduler.java
+++ src/main/java/com/haothink/leetcode/merge_intervals/TaskScheduler.java
@@ -1,5 +1,9 @@
 package com.haothink.leetcode.merge_intervals;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * Created by wanghao on 2021-04-05
  *
@@ -18,17 +22,58 @@ package com.haothink.leetcode.merge_intervals;
  * Explanation:
  * A -> B -> idle -> A -> B -> idle -> A -> B
  * There is at least 2 units of time between any two same tasks.
+ * AAAABBBEEFFGG 3
+ *
+ * A出现了4次，最多，mx=4，那么可以分为mx-1=3块，如下：
+ * A---A---A---
+ * 每块有n+1=4个，最后还要加上末尾的一个A，也就是25-24=1个任务，最终结果为13：
+ * ABEFABEGABFGA
  **/
 public class TaskScheduler {
 
   public static void main(String[] args) {
+
+
+    TaskScheduler taskScheduler = new TaskScheduler();
+
+    char[] tasks = {'A','A','A','B','B','B','C','C','C','D','D','E'};
+    int n = 2;
+    System.out.println(taskScheduler.leastInterval(tasks,n));
 
   }
 
   public int leastInterval(char[] tasks, int n) {
 
 
-    return 0;
+    //statistic numbers of same task
+    Map<Character,Integer> charMap = new HashMap<>();
+
+    for(int i=0;i<tasks.length;i++){
+
+      if(charMap.containsKey(tasks[i])){
+
+        charMap.put(tasks[i],charMap.get(tasks[i])+1);
+      }else {
+
+        charMap.put(tasks[i],1);
+      }
+    }
+    int maxNum = 0;
+    int m = 0;
+    for (Map.Entry<Character,Integer> entry : charMap.entrySet()){
+
+      if(entry.getValue() > maxNum){
+
+        maxNum = entry.getValue();
+        m = 1;
+      }else if(entry.getValue() == maxNum){
+        m ++;
+      }
+    }
+
+    int len = (maxNum-1)*(n+1)+m;
+
+    return Math.max(len,tasks.length);
   }
 
 }
