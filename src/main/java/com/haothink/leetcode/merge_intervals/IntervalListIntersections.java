71ac44118044446cdf353bbdb1f139a5701f5f37
wanghaoListening <wanghaotime@gmail.com>
2021-04-09 16:22:31 +0800

    Interval List Intersections

---
+++ src/main/java/com/haothink/leetcode/merge_intervals/IntervalListIntersections.java
@@ -0,0 +1,36 @@
+package com.haothink.leetcode.merge_intervals;
+
+/**
+ * Created by wanghao on 2021-04-09
+ *
+ * You are given two lists of closed intervals, firstList and secondList,
+ * where firstList[i] = [starti, endi] and secondList[j] = [startj, endj].
+ * Each list of intervals is pairwise disjoint and in sorted order.
+ *
+ * Return the intersection of these two interval lists.
+ *
+ * A closed interval [a, b] (with a < b) denotes the set of real numbers x with a <= x <= b.
+ *
+ * The intersection of two closed intervals is a set of real numbers that are either empty
+ * or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+ **/
+public class IntervalListIntersections {
+
+
+  public static void main(String[] args) {
+
+
+    IntervalListIntersections intervalListIntersections = new IntervalListIntersections();
+
+
+  }
+
+
+  public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+
+
+
+    return null;
+  }
+
+}
