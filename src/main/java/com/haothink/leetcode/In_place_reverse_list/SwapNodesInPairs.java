51f8f4be4ae9ec5a658cc49ddb4f0c86e3dfa832
wanghaoListening <wanghaotime@gmail.com>
2021-04-09 17:30:35 +0800

    Swap Nodes In Pairs

---
+++ src/main/java/com/haothink/leetcode/In_place_reverse_list/SwapNodesInPairs.java
@@ -0,0 +1,26 @@
+package com.haothink.leetcode.In_place_reverse_list;
+
+/**
+ * Created by wanghao on 2021-04-09
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ **/
+public class SwapNodesInPairs {
+
+
+  public ListNode swapPairs(ListNode head) {
+
+
+    return null;
+  }
+
+
+
+  public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
