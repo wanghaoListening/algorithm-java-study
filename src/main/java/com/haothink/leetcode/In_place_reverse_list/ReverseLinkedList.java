29e003c6bf8f22f84a8a2ee43ddcc514fc682c1d
wanghaoListening <wanghaotime@gmail.com>
2021-04-15 23:13:38 +0800

    Reverse Linked List

--- src/main/java/com/haothink/leetcode/In_place_reverse_list/ReverseLinkedList.java
+++ src/main/java/com/haothink/leetcode/In_place_reverse_list/ReverseLinkedList.java
@@ -1,5 +1,7 @@
 package com.haothink.leetcode.In_place_reverse_list;
 
+import java.util.Objects;
+
 /**
  * Created by wanghao on 2021-04-15
  **/
@@ -8,9 +10,53 @@ public class ReverseLinkedList {
 
   public static void main(String[] args) {
 
+
+    ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+
+    ListNode one = new ListNode(1);
+    ListNode two = new ListNode(2);
+    ListNode three = new ListNode(3);
+    ListNode four = new ListNode(4);
+    ListNode five = new ListNode(5);
+
+
+    one.next = two;
+    two.next = three;
+    three.next = four;
+    four.next = five;
+
+    reverseLinkedList.reverseList(one);
+
   }
 
 
+  public ListNode reverseList(ListNode head) {
+
+    if(Objects.isNull(head) || Objects.isNull(head.next)){
+
+      return head;
+    }
+
+    ListNode newHead = null;
+    ListNode pointer = head;
+    ListNode currNode = null;
+    while (Objects.nonNull(pointer)){
+
+      currNode = pointer;
+      pointer = pointer.next;
+      currNode.next = null;
+      if(Objects.isNull(newHead)){
+        newHead = currNode;
+
+      }else {
+        currNode.next = newHead;
+        newHead = currNode;
+      }
+
+    }
+
+    return newHead;
+  }
 
 
   public static class ListNode {
