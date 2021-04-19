3f1c9a654e83350abaf0cdb5f09c7f7f25cc967e
wanghaoListening <wanghaotime@gmail.com>
2021-04-19 23:45:37 +0800

    Implement Trie

---
+++ src/main/java/com/haothink/leetcode/tree_depth_first_search/ImplementTrie.java
@@ -0,0 +1,52 @@
+package com.haothink.leetcode.tree_depth_first_search;
+
+/**
+ * Created by wanghao on 2021-04-19
+ *
+ *
+ * A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store
+ * and retrieve keys in a dataset of strings. There are various applications of this data structure,
+ * such as autocomplete and spellchecker.
+ *
+ * Implement the Trie class:
+ *
+ * Trie() Initializes the trie object.
+ * void insert(String word) Inserts the string word into the trie.
+ * boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+ * boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix
+ * prefix, and false otherwise.
+ *
+ **/
+public class ImplementTrie {
+
+
+
+
+
+  class Trie {
+
+    /** Initialize your data structure here. */
+    public Trie() {
+
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+
+      return false;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+
+
+      return false;
+    }
+  }
+
+}
