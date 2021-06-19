2d7689f75d82a81ebda1eb832698e1ff21cd38ce
wanghaoListening <wanghaotime@gmail.com>
2021-06-19 23:50:17 +0800

    Kth Smallest Element In A BST

---
+++ src/main/java/com/haothink/leetcode/tree_depth_first_search/KthSmallestElementInABST.java
@@ -0,0 +1,37 @@
+package com.haothink.leetcode.tree_depth_first_search;
+
+/**
+ * Created by wanghao on 2021-06-19
+ **/
+public class KthSmallestElementInABST {
+
+  public static void main(String[] args) {
+
+  }
+
+  public int kthSmallest(TreeNode root, int k) {
+
+    return 0;
+  }
+
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+}
