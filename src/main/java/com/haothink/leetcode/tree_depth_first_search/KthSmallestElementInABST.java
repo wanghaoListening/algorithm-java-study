90276e2bd81637d6a22d9531822b205abf6cbfec
wanghaoListening <wanghaotime@gmail.com>
2021-06-20 13:04:33 +0800

    Kth Smallest Element In ABST

--- src/main/java/com/haothink/leetcode/tree_depth_first_search/KthSmallestElementInABST.java
+++ src/main/java/com/haothink/leetcode/tree_depth_first_search/KthSmallestElementInABST.java
@@ -1,7 +1,16 @@
 package com.haothink.leetcode.tree_depth_first_search;
 
+import java.util.Objects;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  * Created by wanghao on 2021-06-19
+ *
+ * Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+ *
+ *
  **/
 public class KthSmallestElementInABST {
 
@@ -11,8 +20,34 @@ public class KthSmallestElementInABST {
 
   public int kthSmallest(TreeNode root, int k) {
 
-    return 0;
+    SortedSet<Integer> set = new TreeSet<>();
+
+    dfsPreOrder(root,set,k);
+
+    return set.last();
   }
+
+  private void dfsPreOrder(TreeNode root, SortedSet<Integer> set,int k) {
+
+    if(Objects.isNull(root)){
+
+      return;
+    }
+
+    if(set.size() >= k){
+      //remove the largest ele in the set
+      if(root.val < set.last()){
+        set.remove(set.last());
+        set.add(root.val);
+      }
+    }else{
+      set.add(root.val);
+    }
+
+    dfsPreOrder(root.left,set,k);
+    dfsPreOrder(root.right,set,k);
+  }
+
 
   public class TreeNode {
 
