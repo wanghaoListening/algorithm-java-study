22102823a6a7ac8d49b3fad7e790cda92e284705
wanghaoListening <wanghaotime@gmail.com>
2021-06-27 22:46:46 +0800

    Implement Queue Using Stacks

---
+++ src/main/java/com/haothink/shoppe/ImplementQueueUsingStacks.java
@@ -0,0 +1,24 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-06-27
+ *
+ * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all
+ * the functions of a normal queue (push, peek, pop, and empty).
+ *
+ * Implement the MyQueue class:
+ *
+ * void push(int x) Pushes element x to the back of the queue.
+ * int pop() Removes the element from the front of the queue and returns it.
+ * int peek() Returns the element at the front of the queue.
+ * boolean empty() Returns true if the queue is empty, false otherwise.
+ **/
+public class ImplementQueueUsingStacks {
+
+  public static void main(String[] args) {
+
+  }
+
+
+
+}
