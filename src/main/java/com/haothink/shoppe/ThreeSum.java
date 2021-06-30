11e9a83ae7aed9e4727f1ccf4bf44900dfbd58d4
wanghaoListening <wanghaotime@gmail.com>
2021-06-30 20:05:45 +0800

    ThreeSum

---
+++ src/main/java/com/haothink/shoppe/ThreeSum.java
@@ -0,0 +1,20 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-06-30
+ *
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ **/
+public class ThreeSum {
+
+  public static void main(String[] args) {
+
+  }
+
+
+}
