82a8963f42c2f04c7a8966c14bb3ea990b7054ba
wanghaoListening <wanghaotime@gmail.com>
2021-06-30 21:31:43 +0800

    MergeTwoSortedLists

--- src/main/java/com/haothink/shoppe/MergeTwoSortedLists.java
+++ src/main/java/com/haothink/shoppe/MergeTwoSortedLists.java
@@ -1,5 +1,7 @@
 package com.haothink.shoppe;
 
+import java.util.Objects;
+
 /**
  * Created by wanghao on 2021-06-30
  *
@@ -12,11 +14,40 @@ public class MergeTwoSortedLists {
 
   public static void main(String[] args) {
 
+
+
   }
 
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-    return null;
+    if(Objects.isNull(l1) || Objects.isNull(l2)){
+
+      return Objects.nonNull(l1)?l1:l2;
+    }
+    ListNode head = new ListNode(-1);
+    ListNode headPointer = head;
+    ListNode onePointer = l1;
+    ListNode twoPointer = l2;
+
+    while (Objects.nonNull(onePointer) || Objects.nonNull(twoPointer)){
+
+      if(Objects.isNull(onePointer) || Objects.isNull(twoPointer)){
+
+        head.next = Objects.isNull(onePointer) ? twoPointer:onePointer;
+        break;
+      }
+      if(onePointer.val <= twoPointer.val){
+        head.next = onePointer;
+        head = head.next;
+        onePointer = onePointer.next;
+      }else {
+        head.next = twoPointer;
+        head = head.next;
+        twoPointer = twoPointer.next;
+      }
+    }
+
+    return headPointer.next;
   }
 
   public class ListNode {
