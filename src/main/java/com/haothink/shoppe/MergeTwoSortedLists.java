119ffec26b4e701874202f578b36502ed542446d
wanghaoListening <wanghaotime@gmail.com>
2021-06-30 20:57:20 +0800

    MergeTwoSortedLists

---
+++ src/main/java/com/haothink/shoppe/MergeTwoSortedLists.java
@@ -0,0 +1,27 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-06-30
+ *
+ * Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+ *
+ * Input: l1 = [1,2,4], l2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ **/
+public class MergeTwoSortedLists {
+
+  public static void main(String[] args) {
+
+  }
+
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    return null;
+  }
+
+  public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+  }
+}
