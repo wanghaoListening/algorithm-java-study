7fc87e5127887df30bef177b2437de6bff3faf9e
wanghaoListening <wanghaotime@gmail.com>
2021-07-01 15:15:26 +0800

    Longest Common Prefix

---
+++ src/main/java/com/haothink/shoppe/LongestCommonPrefix.java
@@ -0,0 +1,33 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-01
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ **/
+public class LongestCommonPrefix {
+
+
+  public static void main(String[] args) {
+
+  }
+
+
+  public String longestCommonPrefix(String[] strs) {
+
+
+    String prefix = "";
+
+
+    return "";
+  }
+
+}
