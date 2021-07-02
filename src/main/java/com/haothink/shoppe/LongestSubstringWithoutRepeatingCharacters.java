d869dc38972c25836494a7a4b6c9ca8dc3795539
wanghaoListening <wanghaotime@gmail.com>
2021-07-02 15:42:56 +0800

    Longest Substring Without Repeating Characters

---
+++ src/main/java/com/haothink/shoppe/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,26 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-02
+ *
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ **/
+public class LongestSubstringWithoutRepeatingCharacters {
+
+
+  public static void main(String[] args) {
+
+  }
+
+
+  public int lengthOfLongestSubstring(String s) {
+
+
+    return 0;
+  }
+
+}
