6ebd33c426025ee1e43a524dd4303df671b9302e
wanghaoListening <wanghaotime@gmail.com>
2021-07-02 14:45:25 +0800

    Longest Common Prefix

--- src/main/java/com/haothink/shoppe/LongestCommonPrefix.java
+++ src/main/java/com/haothink/shoppe/LongestCommonPrefix.java
@@ -1,5 +1,8 @@
 package com.haothink.shoppe;
 
+import java.util.Objects;
+import java.util.Observable;
+
 /**
  * Created by wanghao on 2021-07-01
  * Write a function to find the longest common prefix string amongst an array of strings.
@@ -18,16 +21,37 @@ public class LongestCommonPrefix {
 
   public static void main(String[] args) {
 
+    String[] strs = {"flower","flow","flight"};
+
+    LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+
+    System.out.println(longestCommonPrefix.longestCommonPrefix(strs));
   }
 
 
   public String longestCommonPrefix(String[] strs) {
 
+    if(Objects.isNull(strs) || strs.length == 0){
 
-    String prefix = "";
+      return "";
+    }
 
+    StringBuilder stringBuilder = new StringBuilder();
+    char prefixChar;
 
-    return "";
+    for(int i=0;i<strs[0].length();i++){
+
+      prefixChar = strs[0].charAt(i);
+      for(String str : strs){
+
+        if(str.length()-1 < i || str.charAt(i) != prefixChar){
+          return stringBuilder.toString();
+        }
+      }
+      stringBuilder.append(prefixChar);
+    }
+
+    return stringBuilder.toString();
   }
 
 }
