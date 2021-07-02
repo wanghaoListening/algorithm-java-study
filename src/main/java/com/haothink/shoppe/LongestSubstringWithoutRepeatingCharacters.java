26578ff481a51c226741a2a489b0c4ca333103f3
wanghaoListening <wanghaotime@gmail.com>
2021-07-02 18:50:42 +0800

    Longest Substring Without Repeating Characters

--- src/main/java/com/haothink/shoppe/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/haothink/shoppe/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package com.haothink.shoppe;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by wanghao on 2021-07-02
  *
@@ -19,8 +22,20 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
   public int lengthOfLongestSubstring(String s) {
 
-
-    return 0;
+    Set<Character> set = new HashSet<>();
+    int n = s.length(), left = 0, right = 0;
+    int result = 0;
+    while (left < n && right < n) {
+      if (!set.contains(s.charAt(right))) {
+        set.add(s.charAt(right));
+        right++;
+        result = Math.max(result, right-left);
+      } else {
+        set.remove(s.charAt(left));
+        left++;
+      }
+    }
+    return result;
   }
 
 }
