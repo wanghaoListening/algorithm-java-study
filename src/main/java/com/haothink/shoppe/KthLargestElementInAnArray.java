ff0dd069941bf64c71816e87daedd519054e48df
wanghaoListening <wanghaotime@gmail.com>
2021-07-04 15:13:13 +0800

    KthLargestElementInAnArray

---
+++ src/main/java/com/haothink/shoppe/KthLargestElementInAnArray.java
@@ -0,0 +1,29 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-04
+ *
+ * Given an integer array nums and an integer k, return the kth largest element in the array.
+ *
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ *
+ * Input: nums = [3,2,1,5,6,4], k = 2
+ * Output: 5
+ *
+ * Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+ * Output: 4
+ **/
+public class KthLargestElementInAnArray {
+
+
+  public static void main(String[] args) {
+
+  }
+
+  public int findKthLargest(int[] nums, int k) {
+
+
+    return 0;
+  }
+
+}
