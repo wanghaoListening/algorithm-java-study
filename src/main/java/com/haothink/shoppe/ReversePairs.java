47e435bae5217ad10a0f83d86ec03005d4efcbea
wanghaoListening <wanghaotime@gmail.com>
2021-07-05 13:27:23 +0800

    ReversePairs

---
+++ src/main/java/com/haothink/shoppe/ReversePairs.java
@@ -0,0 +1,24 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-05
+ *
+ * Given an integer array nums, return the number of reverse pairs in the array.
+ *
+ * A reverse pair is a pair (i, j) where 0 <= i < j < nums.length and nums[i] > 2 * nums[j].
+ *
+ * 难度：hard
+ **/
+public class ReversePairs {
+
+
+  public static void main(String[] args) {
+
+  }
+
+  public int reversePairs(int[] nums) {
+
+    return 0;
+  }
+
+}
