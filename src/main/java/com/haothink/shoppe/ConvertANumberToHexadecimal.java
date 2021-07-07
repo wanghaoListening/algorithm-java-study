74aa311968531cbb5a6411374179b4c603ec8007
wanghaoListening <wanghaotime@gmail.com>
2021-07-07 13:27:06 +0800

    First Bad Version

---
+++ src/main/java/com/haothink/shoppe/ConvertANumberToHexadecimal.java
@@ -0,0 +1,30 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-07
+ *
+ * Given an integer num, return a string representing its hexadecimal representation. For negative integers,
+ * two’s complement method is used.
+ *
+ * All the letters in the answer string should be lowercase characters, and there should not be any leading
+ * zeros in the answer except for the zero itself.
+ *
+ * Note: You are not allowed to use any built-in library method to directly solve this problem.
+ *
+ * Input: num = 26
+ * Output: "1a"
+ **/
+public class ConvertANumberToHexadecimal {
+
+
+  public static void main(String[] args) {
+
+  }
+
+  public String toHex(int num) {
+
+
+    return null;
+  }
+
+}
