991bf891ab002a4757c372a6004838227967bd8f
wanghaoListening <wanghaotime@gmail.com>
2021-07-09 00:35:23 +0800

    RemoveDuplicatesFromSortedListII

---
+++ src/main/java/com/haothink/shoppe/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,34 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-09
+ *
+ * Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct
+ * numbers from the original list. Return the linked list sorted as well.
+ *
+ * Input: head = [1,2,3,3,4,4,5]
+ * Output: [1,2,5]
+ **/
+public class RemoveDuplicatesFromSortedListII {
+
+
+  public static void main(String[] args) {
+
+  }
+
+  public ListNode deleteDuplicates(ListNode head) {
+
+
+    return null;
+  }
+
+
+  public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+}
