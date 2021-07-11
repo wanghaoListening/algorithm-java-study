c0397ed3322e69e40fdbd95ec50e622d7e088802
wanghaoListening <wanghaotime@gmail.com>
2021-07-11 18:47:15 +0800

    Intersection Of Two Arrays II

--- src/main/java/com/haothink/shoppe/IntersectionOfTwoArraysII.java
+++ src/main/java/com/haothink/shoppe/IntersectionOfTwoArraysII.java
@@ -1,5 +1,11 @@
 package com.haothink.shoppe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by wanghao on 2021-07-10
  *
@@ -18,6 +24,13 @@ public class IntersectionOfTwoArraysII {
 
   public static void main(String[] args) {
 
+    int[] nums1 = {4,9,5};
+
+    int[] nums2 = {9,4,9,8,4};
+
+    IntersectionOfTwoArraysII intersection =  new IntersectionOfTwoArraysII();
+
+    System.out.println(Arrays.toString(intersection.intersect(nums1,nums2)));
   }
 
 
@@ -25,9 +38,50 @@ public class IntersectionOfTwoArraysII {
 
   public int[] intersect(int[] nums1, int[] nums2) {
 
+    int[] maxNum = nums1.length > nums2.length ? nums1:nums2;
 
+    int[] minNum = nums1.length > nums2.length ? nums2:nums1;
 
-    return null;
+    Map<Integer,Integer> baselineMap = new HashMap<>();
+
+    for(int i=0;i<maxNum.length;i++){
+
+      int key = maxNum[i];
+      if(baselineMap.containsKey(key)){
+
+        baselineMap.put(key,baselineMap.get(key)+1);
+      }else {
+
+        baselineMap.put(key,1);
+      }
+    }
+
+    List<Integer> numList = new ArrayList<>();
+
+    for(int i=0;i<minNum.length;i++){
+
+      if(!baselineMap.containsKey(minNum[i])){
+
+        continue;
+      }
+
+      int numCount = baselineMap.get(minNum[i]);
+      if(numCount > 1){
+        baselineMap.put(minNum[i],--numCount);
+      }else {
+        baselineMap.remove((minNum[i]));
+      }
+      numList.add(minNum[i]);
+    }
+
+    int[] result = new int[numList.size()];
+
+    for(int i=0;i<numList.size();i++){
+
+      result[i] = numList.get(i);
+    }
+
+    return result;
   }
 
 }
