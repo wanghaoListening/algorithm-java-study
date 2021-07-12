fc3024551a006a04792f6a5845a84d1ef6925962
wanghaoListening <wanghaotime@gmail.com>
2021-07-12 22:46:56 +0800

    Implement Stack Using Queues

---
+++ src/main/java/com/haothink/shoppe/ImplementStackUsingQueues.java
@@ -0,0 +1,71 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-12
+ *
+ * Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+ *
+ * Implement the MyStack class:
+ *
+ * void push(int x) Pushes element x to the top of the stack.
+ * int pop() Removes the element on the top of the stack and returns it.
+ * int top() Returns the element on the top of the stack.
+ * boolean empty() Returns true if the stack is empty, false otherwise.
+ * Notes:
+ *
+ * You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+ * Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as
+ * you use only a queue's standard operations.
+ *
+ * Input
+ * ["MyStack", "push", "push", "top", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * Output
+ * [null, null, null, 2, 2, false]
+ *
+ * Explanation
+ * MyStack myStack = new MyStack();
+ * myStack.push(1);
+ * myStack.push(2);
+ * myStack.top(); // return 2
+ * myStack.pop(); // return 2
+ * myStack.empty(); // return False
+ *
+ **/
+public class ImplementStackUsingQueues {
+
+
+  public static void main(String[] args) {
+
+  }
+
+
+  /** Initialize your data structure here. */
+  public ImplementStackUsingQueues() {
+
+  }
+
+  /** Push element x onto stack. */
+  public void push(int x) {
+
+  }
+
+  /** Removes the element on top of the stack and returns that element. */
+  public int pop() {
+
+    return 0;
+  }
+
+  /** Get the top element. */
+  public int top() {
+
+    return 0;
+  }
+
+  /** Returns whether the stack is empty. */
+  public boolean empty() {
+
+    return false;
+  }
+
+}
