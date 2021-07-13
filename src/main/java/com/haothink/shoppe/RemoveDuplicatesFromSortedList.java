b628d0f7e63261ff49d421cc8cd4cb058915f17f
wanghaoListening <wanghaotime@gmail.com>
2021-07-13 20:21:46 +0800

    Remove Duplicates From Sorted List

---
+++ src/main/java/com/haothink/shoppe/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,19 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021-07-13
+ *
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+ *
+ * Input: head = [1,1,2]
+ * Output: [1,2]
+ **/
+public class RemoveDuplicatesFromSortedList {
+
+  public static void main(String[] args) {
+
+  }
+
+
+
+}
