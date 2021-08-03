8f00c85b92d3d481881a9a0aad17f1e7a8b733b8
wanghaoListening <wanghaotime@gmail.com>
2021-08-03 17:03:38 +0800

    LongestIncreasingSubsequence

---
+++ src/main/java/com/haothink/shoppe/LongestIncreasingSubsequence.java
@@ -0,0 +1,32 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021/8/3
+ *
+ * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+ *
+ * A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements.
+ * For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ *
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ *
+ * Input: nums = [0,1,0,3,2,3]
+ * Output: 4
+ *
+ * 动态规划：
+ **/
+public class LongestIncreasingSubsequence {
+
+  public static void main(String[] args) {
+
+  }
+
+  public int lengthOfLIS(int[] nums) {
+
+
+
+    return 0;
+  }
+}
