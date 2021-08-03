2876d8c6503774ea7a4010a57cbe8fd15b531070
wanghaoListening <wanghaotime@gmail.com>
2021-08-03 13:38:26 +0800

    FindPeakElement

---
+++ src/main/java/com/haothink/shoppe/FindPeakElement.java
@@ -0,0 +1,43 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021/8/2 mail:hiwanghao@didiglobal.com
+ * A peak element is an element that is strictly greater than its neighbors.
+ *
+ * Given an integer array nums, find a peak element, and return its index. If the array contains
+ * multiple peaks, return the index to any of the peaks.
+ *
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ *
+ * You must write an algorithm that runs in O(log n) time.
+ *
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index number 2.
+ *
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 5
+ * Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+ *
+ **/
+public class FindPeakElement {
+
+  public static void main(String[] args) {
+
+  }
+
+  public int findPeakElement(int[] nums) {
+
+    if(nums.length <3){
+      return 0;
+    }
+    int left = 1, right = nums.length - 2;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < nums[mid + 1]) left = mid + 1;
+      else right = mid;
+    }
+    return right;
+  }
+
+}
---
+++ src/main/java/com/haothink/shoppe/RandomPickWithWeight.java
@@ -0,0 +1,37 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021/8/2 mail:hiwanghao@didiglobal.com
+ *
+ * You are given an array of positive integers w where w[i] describes the weight of ith index (0-indexed).
+ *
+ * We need to call the function pickIndex() which randomly returns an integer in the range [0, w.length - 1].
+ * pickIndex() should return the integer proportional to its weight in the w array. For example, for w = [1, 3],
+ * the probability of picking the index 0 is 1 / (1 + 3) = 0.25 (i.e 25%) while the probability of picking the
+ * index 1 is 3 / (1 + 3) = 0.75 (i.e 75%).
+ *
+ * More formally, the probability of picking index i is w[i] / sum(w).
+ *
+ * Input
+ * ["Solution","pickIndex"]
+ * [[[1]],[]]
+ * Output
+ * [null,0]
+ **/
+public class RandomPickWithWeight {
+
+
+  public static void main(String[] args) {
+
+  }
+
+
+  public RandomPickWithWeight(int[] w) {
+
+  }
+
+  public int pickIndex() {
+
+    return 0;
+  }
+}
