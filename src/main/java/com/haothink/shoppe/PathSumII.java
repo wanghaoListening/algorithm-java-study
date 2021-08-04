f3af2d04481da47b7468e2ce076b114a3524704d
wanghaoListening <wanghaotime@gmail.com>
2021-08-04 11:11:44 +0800

    Path Sum II

---
+++ src/main/java/com/haothink/shoppe/PathSumII.java
@@ -0,0 +1,74 @@
+package com.haothink.shoppe;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by wanghao on 2021/8/3 mail:
+ *
+ * Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
+ *
+ * A leaf is a node with no children.
+ *
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * Output: [[5,4,11,2],[5,8,4,5]]
+ *
+ **/
+public class PathSumII {
+
+
+  public static void main(String[] args) {
+
+
+  }
+
+
+  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+
+
+    List<List<Integer>> results = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
+
+    dfsFindPathSum(root,targetSum,results,list);
+
+    return results;
+
+  }
+
+  public void dfsFindPathSum(TreeNode root, int targetSum,List<List<Integer>> sumList,List<Integer> list){
+
+    if(Objects.isNull(root)){
+
+      return;
+    }
+
+    if(Objects.isNull(root.left) && Objects.isNull(root.right) && root.val == targetSum){
+
+      list.add(root.val);
+      sumList.add(list);
+      return;
+    }
+
+    list.add(root.val);
+    dfsFindPathSum(root.left,targetSum-root.val,sumList,new ArrayList<>(list));
+    dfsFindPathSum(root.right,targetSum-root.val,sumList,new ArrayList<>(list));
+    list.remove(list.size()-1);
+  }
+
+
+
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
