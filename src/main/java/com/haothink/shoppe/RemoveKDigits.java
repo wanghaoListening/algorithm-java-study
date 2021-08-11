defc59eae4ee2d81ca2ec738312c1a4bd0743966
wanghaoListening <wanghaotime@gmail.com>
2021-08-11 16:37:26 +0800

    quick sort

---
+++ src/main/java/com/haothink/shoppe/RemoveKDigits.java
@@ -0,0 +1,29 @@
+package com.haothink.shoppe;
+
+/**
+ * Created by wanghao on 2021/8/8
+ *
+ * Given string num representing a non-negative integer num, and an integer k, return the smallest
+ * possible integer after removing k digits from num.
+ *
+ * Input: num = "1432219", k = 3
+ * Output: "1219"
+ * Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+ *
+ * Input: num = "10200", k = 1
+ * Output: "200"
+ * Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+ **/
+public class RemoveKDigits {
+
+  public static void main(String[] args) {
+
+  }
+
+  
+  public String removeKdigits(String num, int k) {
+
+
+    return null;
+  }
+}
