7c8e648591760b4bbee0c8eb0adbe900cc47c502
wanghaoListening <wanghaotime@gmail.com>
2021-08-18 16:31:23 +0800

    Basic Calculator

---
+++ src/main/java/com/haothink/tencent/BasicCalculator.java
@@ -0,0 +1,30 @@
+package com.haothink.tencent;
+
+/**
+ * Created by wanghao on 2021/8/18
+ *
+ * Given a string s representing a valid expression, implement a basic calculator to evaluate it,
+ * and return the result of the evaluation.
+ *
+ * Note: You are not allowed to use any built-in function which evaluates strings as mathematical
+ * expressions, such as eval().
+ *
+ * Input: s = "1 + 1"
+ * Output: 2
+ *
+ * Input: s = " 2-1 + 2 "
+ * Output: 3
+ *
+ * 乘除直接计算，加减进队列
+ *
+ * hard ----
+ **/
+public class BasicCalculator {
+
+
+  public int calculate(String s) {
+
+
+    return 0;
+  }
+}
