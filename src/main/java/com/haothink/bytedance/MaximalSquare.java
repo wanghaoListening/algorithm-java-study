7512906cfe18b8077ba7e62a84b232cdc4f06cce
wanghaoListening <wanghaotime@gmail.com>
2021-09-02 15:59:27 +0800

    Maximal Square

---
+++ src/main/java/com/haothink/bytedance/MaximalSquare.java
@@ -0,0 +1,27 @@
+package com.haothink.bytedance;
+
+/**
+ * Created by wanghao on 2021/9/2
+ *
+ * Given an m x n binary matrix filled with 0's and 1's, find the largest
+ * square containing only 1's and return its area.
+ *
+ * Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * Output: 4
+ *
+ * Input: matrix = [["0","1"],["1","0"]]
+ * Output: 1
+ *
+ * 一般求解最值的问题都可以朝着动态规划的方向去想。
+ * 解题思路
+ * https://leetcode-cn.com/problems/maximal-square/solution/221-zui-da-zheng-fang-xing-tu-jie-shi-pin-yan-shi-/
+ **/
+public class MaximalSquare {
+
+
+  public int maximalSquare(char[][] matrix) {
+
+
+    return 0;
+  }
+}
--- src/main/java/com/haothink/bytedance/ValidateBinarySearchTree.java
+++ src/main/java/com/haothink/bytedance/ValidateBinarySearchTree.java
@@ -2,7 +2,7 @@ package com.haothink.bytedance;
 
 /**
  * Created by wanghao on 2021/9/2
- * 
+ *
  **/
 public class ValidateBinarySearchTree {
 
