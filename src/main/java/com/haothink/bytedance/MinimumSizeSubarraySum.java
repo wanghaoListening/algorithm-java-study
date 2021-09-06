559cd5ab02d7e62e92bf4dea16af7f481aa9824a
wanghaoListening <wanghaotime@gmail.com>
2021-09-06 15:26:16 +0800

    MinimumSizeSubarraySum

---
+++ src/main/java/com/haothink/bytedance/MinimumSizeSubarraySum.java
@@ -0,0 +1,26 @@
+package com.haothink.bytedance;
+
+/**
+ * Created by wanghao on 2021/9/6
+ *
+ *
+ Given an array of positive integers nums and a positive integer target, return the minimal
+ length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is
+ greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ Input: target = 7, nums = [2,3,1,2,4,3]
+ Output: 2
+ Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+ **/
+public class MinimumSizeSubarraySum {
+
+
+  public static void main(String[] args) {
+
+  }
+
+  public int minSubArrayLen(int target, int[] nums) {
+
+    return 0;
+  }
+}
