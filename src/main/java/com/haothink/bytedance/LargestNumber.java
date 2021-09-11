81f6308537da61fb178b27afbd18a98375acfe84
wanghaoListening <wanghaotime@gmail.com>
2021-09-11 12:21:40 +0800

    Largest Number

---
+++ src/main/java/com/haothink/bytedance/LargestNumber.java
@@ -0,0 +1,18 @@
+package com.haothink.bytedance;
+
+/**
+ * Created by wanghao on 2021/9/11
+ *
+ * Given a list of non-negative integers nums, arrange them such that they form the largest number.
+ *
+ * Note: The result may be very large, so you need to return a string instead of an integer.
+ *
+ * Input: nums = [10,2]
+ * Output: "210"
+ *
+ * Input: nums = [3,30,34,5,9]
+ * Output: "9534330"
+ **/
+public class LargestNumber {
+
+}
