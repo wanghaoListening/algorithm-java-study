4ad70b621519328b016df77957962f0c8ba9b579
wanghaoListening <wanghaotime@gmail.com>
2021-09-16 16:36:52 +0800

    Three Sum Closest

---
+++ src/main/java/com/haothink/bytedance/ThreeSumClosest.java
@@ -0,0 +1,21 @@
+package com.haothink.bytedance;
+
+/**
+ * Created by wanghao on 2021/9/16
+ *
+ * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+ *
+ * Return the sum of the three integers.
+ *
+ * You may assume that each input would have exactly one solution.
+ *
+ * Input: nums = [-1,2,1,-4], target = 1
+ * Output: 2
+ * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ **/
+public class ThreeSumClosest {
+
+  public static void main(String[] args) {
+
+  }
+}
