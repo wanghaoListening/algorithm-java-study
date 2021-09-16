964717526d385c11cfbdbd9ab9803ff5e0ef7537
wanghaoListening <wanghaotime@gmail.com>
2021-09-16 14:39:25 +0800

    Rotate List

---
+++ src/main/java/com/haothink/bytedance/RotateList.java
@@ -0,0 +1,13 @@
+package com.haothink.bytedance;
+
+/**
+ * Created by wanghao on 2021/9/16
+ *
+ * Given the head of a linked list, rotate the list to the right by k places.
+ *
+ * Input: head = [1,2,3,4,5], k = 2
+ * Output: [4,5,1,2,3]
+ **/
+public class RotateList {
+
+}
