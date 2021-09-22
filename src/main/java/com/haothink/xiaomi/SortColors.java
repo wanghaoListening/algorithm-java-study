7b7ae64ac18a21d84068b3d41bd525f633950c2f
wanghaoListening <wanghaotime@gmail.com>
2021-09-22 15:56:06 +0800

    Sort Colors小米

---
+++ src/main/java/com/haothink/xiaomi/SortColors.java
@@ -0,0 +1,13 @@
+package com.haothink.xiaomi;
+
+/**
+ * Created by wanghao on 2021/9/22
+ *
+ * - 定义 red 指针指向开头位置，blue 指针指向末尾位置。
+ *
+ * - 从头开始遍历原数组，如果遇到0，则交换该值和 red 指针指向的值，并将 red 指针后移一位。若遇到2，则交换该值和 blue
+ * 指针指向的值，并将 blue 指针前移一位。若遇到1，则继续遍历。
+ **/
+public class SortColors {
+
+}
