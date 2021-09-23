2d9522d7f506f0d907781ad20446dada359c2c8a
wanghaoListening <wanghaotime@gmail.com>
2021-09-23 23:14:17 +0800

    Three Sum 快手

---
+++ src/main/java/com/haothink/kuaishou/ThreeSum.java
@@ -0,0 +1,25 @@
+package com.haothink.kuaishou;
+
+import java.util.List;
+
+/**
+ * Created by wanghao on 2021/9/23
+ *
+ * 设置三指针
+ *
+ *  * 1.先将数组排序
+ *  *
+ *  * 2.循环遍历数组元素，采用双指针法，如：当前元素下标为 i ，则低指针为  i+1， 高指针为 arr.length - 1 .
+ *  *
+ *  * 3.如果三下标所对应元素的和大于0，则 高指针减小一，如果和小于0则低指针加一， 否则 则等于 0。
+ *  *
+ *  * 4.如果等于0，再判断有没有重复元素。
+ **/
+public class ThreeSum {
+
+  public List<List<Integer>> threeSum(int[] nums) {
+
+
+    return null;
+  }
+}
