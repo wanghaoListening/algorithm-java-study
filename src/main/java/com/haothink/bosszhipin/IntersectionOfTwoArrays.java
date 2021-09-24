1d3089c4a3f2642bc8860529133658bc8f2ae2d1
wanghaoListening <wanghaotime@gmail.com>
2021-09-24 14:43:59 +0800

    IntersectionOfTwoArrays boss直聘

---
+++ src/main/java/com/haothink/bosszhipin/IntersectionOfTwoArrays.java
@@ -0,0 +1,15 @@
+package com.haothink.bosszhipin;
+
+/**
+ * Created by wanghao on 2021/9/23
+ *
+ * a 和 b 两个数组的头部分别维护两个指针，若其中一个比另一个小，则向前移动，若遇到相等时保存，
+ * 遍历直到其中一个数组的尾部，时间复杂度O(m+n)；
+ **/
+public class IntersectionOfTwoArrays {
+
+  public int[] intersection(int[] nums1, int[] nums2) {
+
+    return null;
+  }
+}
