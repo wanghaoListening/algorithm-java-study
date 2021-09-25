4891cd886d4bae18fc13e109e790336213f5df7c
wanghaoListening <wanghaotime@gmail.com>
2021-09-25 15:27:31 +0800

    Multiply Strings 快手

---
+++ src/main/java/com/haothink/kuaishou/MultiplyStrings.java
@@ -0,0 +1,17 @@
+package com.haothink.kuaishou;
+
+/**
+ * Created by wanghao on 2021/9/25
+ *
+ * Given two non-negative integers num1 and num2 represented as strings, return the
+ * product of num1 and num2, also represented as a string.
+ *
+ * Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ *  * 竖式乘法
+ *  * 对于例子中的123 * 456，我们可以通过得到:
+ *  * 3 * 456 + 20 * 456 + 100 *456 = 1368 + 9120 + 45600 = 56088
+ **/
+public class MultiplyStrings {
+
+
+}
