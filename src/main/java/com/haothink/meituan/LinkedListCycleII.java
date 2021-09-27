24210f6982b4b9178805590eadf64c9e556f9463
wanghaoListening <wanghaotime@gmail.com>
2021-09-27 23:23:35 +0800

    Linked List Cycle II 美团

---
+++ src/main/java/com/haothink/meituan/LinkedListCycleII.java
@@ -0,0 +1,12 @@
+package com.haothink.meituan;
+
+/**
+ * Created by wanghao on 2021/9/27
+ *
+ * 给定阶段 1 找到的相遇点，阶段 2 将找到环的入口。首先我们初始化额外的两个指针： ptr1 ，
+ * 指向链表的头， ptr2 指向相遇点。然后，我们每次将它们往前移动一步，直到它们相遇，
+ * 它们相遇的点就是环的入口，返回这个节点。
+ **/
+public class LinkedListCycleII {
+
+}
