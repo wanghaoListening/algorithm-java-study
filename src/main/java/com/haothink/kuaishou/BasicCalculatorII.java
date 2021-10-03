53870f1b017a907c853c561cd9535b45af6f4c34
wanghaoListening <wanghaotime@gmail.com>
2021-10-03 08:59:14 +0800

    Basic Calculator II 快手

---
+++ src/main/java/com/haothink/kuaishou/BasicCalculatorII.java
@@ -0,0 +1,28 @@
+package com.haothink.kuaishou;
+
+/**
+ * Created by wanghao on 2021/9/30
+ *
+ * Given a string s which represents an expression, evaluate this expression and return its value.
+ *
+ * The integer division should truncate toward zero.
+ *
+ * You may assume that the given expression is always valid. All intermediate results will be in
+ * the range of [-231, 231 - 1].
+ *
+ * Note: You are not allowed to use any built-in function which evaluates strings as
+ * mathematical expressions, such as eval().
+ *
+ * Input: s = "3+2*2"
+ * Output: 7
+ *
+ * Input: s = " 3/2 "
+ * Output: 1
+ **/
+public class BasicCalculatorII {
+
+  public int calculate(String s) {
+
+    return 0;
+  }
+}
