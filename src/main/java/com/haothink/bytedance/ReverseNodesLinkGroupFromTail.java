12fa5a22aaf7aea82075793bfd4c428f15405272
wanghao.629 <wanghao.629@bytedance.com>
2023-05-10 00:10:27 +0800

    58同城面试题

--- src/main/java/com/haothink/bytedance/ReverseNodesLinkGroupFromTail.java
+++ src/main/java/com/haothink/bytedance/ReverseNodesLinkGroupFromTail.java
@@ -3,7 +3,7 @@ package com.haothink.bytedance;
 import com.haothink.ListNode;
 
 import java.util.LinkedList;
-import java.util.List;
+
 
 /**
  *  给定一个单链表的头节点 head,实现一个调整单链表的函数，使得每K个节点之间为一组进行逆序，并且从链表的尾部开始组起，头部剩余节点数量不够一组的不需要逆序。
